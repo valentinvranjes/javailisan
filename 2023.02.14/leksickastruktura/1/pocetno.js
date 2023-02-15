253060d4d5fd826851c4ac7e7ab42d241be94f23
Valentin Vranjes <valentin.vranjes@gmail.com>
2023-02-15 16:22:59 +0100

    prve vjezbe

---
+++ 2023.02.14/leksickastruktura/1/pocetno.js
@@ -0,0 +1,21 @@
+var firstname, lastName, price, discount, fullPrice;
+
+firstname = "John";
+lastName = "Doe";
+price = 19.90;
+discount = 0.10;
+
+var x = 1; // a može biti napisano i ovako var = x; x = 1;
+if (x == 1) {
+    x = 2;
+}
+
+var a = true;
+if (a == true) { // var a = false; if (!a = false) ili var a = false; if (a == false)
+    var z = 44;
+    return 0;
+}
+
+if (discount !=0) {
+fullPrice = (price * 100) / discount; // i ne mora sa zagradom, operacije iste važnosti 
+}
